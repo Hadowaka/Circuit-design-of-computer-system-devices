0a463ab4ce281595ad30e6fc9e56d18ffa46eb65
Hadowaka <110410030+Hadowaka@users.noreply.github.com>
2025-09-15 11:43:19 +0300

    Create test.v

---
+++ Reversible counter/test.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module test();
+    reg clk;
+    wire [2:0] out;  // 3 бита т.к. mod=8 ? $clog2(8)=3
+
+    // Создаем экземпляр тестируемого модуля
+    count #(
+        .step(1),
+        .mod(8)
+    ) dut (
+        .clk(clk),
+        .out(out)
+    );
+
+    // Генерация тактового сигнала
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;  // Период 10 нс
+    end
+
+    // Завершение симуляции
+    initial begin
+        #100 $finish;
+    end
+
+endmodule
